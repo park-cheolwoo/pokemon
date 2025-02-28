07a01542502521ecae552c4b77831e1eb392aa96
espionage23 <zalstmd23@naver.com>
2025-02-28 16:57:16 +0900

    Feat: add habitat (api, data)

--- src/main/java/kr/co/pokemon/data/controller/DataController.java
+++ src/main/java/kr/co/pokemon/data/controller/DataController.java
@@ -10,18 +10,23 @@ import org.springframework.web.bind.annotation.RestController;
 import kr.co.pokemon.data.dto.PageDTO;
 import kr.co.pokemon.pokemon.dto.EggGroupDTO;
 import kr.co.pokemon.pokemon.dto.EvolutionTriggerDTO;
+import kr.co.pokemon.pokemon.dto.HabitatDTO;
 import kr.co.pokemon.pokemon.service.EggGroupService;
 import kr.co.pokemon.pokemon.service.EvolutionTriggerService;
+import kr.co.pokemon.pokemon.service.HabitatService;
 
 @RestController
 @RequestMapping(value = "/data")
 public class DataController {
 
 	@Autowired
-	EvolutionTriggerService evolutionTriggerService;
+	private EvolutionTriggerService evolutionTriggerService;
 	
 	@Autowired
-	EggGroupService eggGroupService;
+	private EggGroupService eggGroupService;
+	
+	@Autowired
+	private HabitatService habitatService;
 	
 	@GetMapping(value = "/evolution/trigger")
 	public List<EvolutionTriggerDTO> getEvolutionTrigger(PageDTO page) {
@@ -36,4 +41,12 @@ public class DataController {
 		if (page.getSize() <= 0) page.setSize(10);
 		return eggGroupService.getAll(page);
 	}
+	
+	@GetMapping(value = "/habitat")
+	public List<HabitatDTO> getHabitat(PageDTO page) {
+		if (page.getPage() < 0) page.setPage(0);
+		if (page.getSize() <= 0) page.setSize(10);
+		return habitatService.getAll(page);
+	}
+	
 }
