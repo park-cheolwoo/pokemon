00df9e38e7047ee9044093ec6df1a788cd527194
vhfflxbzmffpdhxkdlem <wonwow55@naver.com>
2025-03-12 19:41:09 +0900

    maincontroller 커밋나서 실험한것

--- src/main/java/kr/co/pokemon/controller/MainController.java
+++ src/main/java/kr/co/pokemon/controller/MainController.java
@@ -17,5 +17,36 @@ public class MainController {
 //		model.addAttribute("pdto",playerDto);
 //		return "index";
 //	}
+	@GetMapping(value = "/friend/flist")
+	public String flist() {
+		return "/friend/flist";
+	}
 
+	
+	@GetMapping("/play/plist")
+	public String plist() {
+		return "/play/plist";
+	}	
+	
+	@GetMapping("/play/quest")
+	public String quest() {
+		return "/play/quest";
+	}
+	
+	@GetMapping("/play/dungeon")
+	public String dungeon() {
+		return "/play/dungeon";
+	}
+
+	@GetMapping("/play/sdungeon")
+	public String sdungeon() {
+		return "/play/sdungeon";
+	}
+
+	
+	@GetMapping("/play/battle")
+	public String battle() {
+		return "/play/battle";
+	}
+	
 }
--- src/main/java/kr/co/pokemon/plan/controller/PlanController.java
+++ src/main/java/kr/co/pokemon/plan/controller/PlanController.java
@@ -6,46 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PlanController {
 
-	@GetMapping(value = "/friend/flist")
-	public String flist() {
-		return "/friend/flist";
-	}
 
-	
-	@GetMapping("/play/plist")
-	public String plist() {
-		return "/play/plist";
-	}	
-	
-	@GetMapping("/play/quest")
-	public String quest() {
-		return "/play/quest";
-	}
-	
-	@GetMapping("/play/dungeon")
-	public String dungeon() {
-		return "/play/dungeon";
-	}
-
-	@GetMapping("/play/sdungeon")
-	public String sdungeon() {
-		return "/play/sdungeon";
-	}
-	
-	
-	
-	
-	
-	
-	@GetMapping("/first/first")
-	public String first() {
-		return "/first/first";
-	}	
-	
-	@GetMapping("/play/battle")
-	public String battle() {
-		return "/play/battle";
-	}
-	
 	
 }
