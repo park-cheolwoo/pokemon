16f40beced138581234d6c636dbb2e74e955cb9a
Park-cheolwoo <bd8860@naver.com>
2025-03-13 11:56:44 +0900

    edit : playerMapper.selectAll(PageRequestDTO pDTO) 함수 추가

--- src/main/java/kr/co/pokemon/player/dao/PlayerMapper.java
+++ src/main/java/kr/co/pokemon/player/dao/PlayerMapper.java
@@ -5,13 +5,15 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
+import kr.co.pokemon.data.dto.PageRequestDTO;
 import kr.co.pokemon.player.dto.FriendDTO;
 import kr.co.pokemon.player.dto.PlayerDTO;
 
 @Mapper
 public interface PlayerMapper {
 
-	List<PlayerDTO> selectAll();
+	List<PlayerDTO> selectAll(PageRequestDTO pDTO);
+	
 	PlayerDTO selectById(int id);
 	
 	void updatePlayer(PlayerDTO player);
--- src/main/java/kr/co/pokemon/player/service/PlayerService.java
+++ src/main/java/kr/co/pokemon/player/service/PlayerService.java
@@ -2,13 +2,14 @@ package kr.co.pokemon.player.service;
 
 import java.util.List;
 
+import kr.co.pokemon.data.dto.PageRequestDTO;
 import kr.co.pokemon.player.dto.FriendDTO;
 //import kr.co.pokemon.player.controller.Player;
 import kr.co.pokemon.player.dto.PlayerDTO;
 
 public interface PlayerService {
 	
-	List<PlayerDTO> findAll(int size, int page);
+	List<PlayerDTO> getAll(PageRequestDTO pDTO);
 	
 	PlayerDTO findById(int id);
 
