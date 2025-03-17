2804aadb58e964fdb70d8f3d66f2ffb495f30fdd
eus1211 <eus1211@naver.com>
2025-03-17 18:34:40 +0900

    Merge branch 'main' of https://github.com/park-cheolwoo/pokemon.git into index

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -139,7 +139,7 @@ $(function() {
 			success: function(response) {
 				location.href = "/";
 			},
-			error: function(xhr, status, error) {
+			error: function(error) {
 				console.error('Error:', error);
 			}
 		});
@@ -162,6 +162,7 @@ $(function() {
 	    updateResourceDisplay();
 	});
 
+	//메인화면 소유 포켓몬 불러오기
 	$(document).ready(function () {
 	    function loadMinePokemons() {
 	        $.ajax({
@@ -170,7 +171,7 @@ $(function() {
 	            dataType: 'json',
 	            success: function (response) {
 	                if (response.status === 'success') {
-	                    renderPokemonImages(response.data); // 데이터를 렌더링
+	                    renderPokemonImages(response.data); 
 	                } else {
 	                    console.error('데이터를 불러오지 못했습니다:', response.message);
 	                }
@@ -180,23 +181,20 @@ $(function() {
 	            }
 	        });
 	    }
-
 	    function renderPokemonImages(images) {
 	        const container = $('#pokemon-images-container');
 	        container.empty();
 
-	        images.forEach(image => {
-	            // 기본적으로 3D 이미지를 사용하고, 없으면 2D 이미지로 대체
-	            const selectedImage = image['3D'] || image['2D']; // 3D가 없을 경우 2D를 사용
-	            const imageHTML = `<img src="${selectedImage}" alt="포켓몬 이미지" class="pokemon-image" />`;
-	            container.append(imageHTML);
-	        });
+			images.forEach((image) => {
+			            const selectedImage = image['3D'] || image['2D']; // 3D 우선 사용, 없으면 2D
+			            const delay = Math.random() * 2 + 's'; // 각 이미지에 0~2초 사이의 랜덤 딜레이 설정
+			            const imageHTML = `<img src="${selectedImage}" alt="포켓몬 이미지" class="pokemon-image" style="animation-delay: ${delay};" />`;
+			            container.append(imageHTML);
+			      });
 	    }
 
 	    loadMinePokemons(); // 포켓몬 이미지 로드
 	});
-
-
 
 	$("#mypageBtn").click(function() { mypage(); });
 	$("#storeBtn").click(function() { store(); });
